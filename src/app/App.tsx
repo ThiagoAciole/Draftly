5f30e7bf6304a0c8e4bcd47e6df9074d528a5685
thiago.aciole <thiago.aciole@ifood.com.br>
2026-07-07 21:46:22 -0300

    feat: wire context providers into App tree

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
+import { WorkspaceProvider } from "../contexts/WorkspaceContext";
+import { TabsProvider } from "../contexts/TabsContext";
+import { FileActionsProvider } from "../contexts/FileActionsContext";
 import { AppShell } from "../components/layout/AppShell";
 
 export function App() {
-  return <AppShell />;
+  return (
+    <WorkspaceProvider>
+      <TabsProvider>
+        <FileActionsProvider>
+          <AppShell />
+        </FileActionsProvider>
+      </TabsProvider>
+    </WorkspaceProvider>
+  );
 }
---
+++ src/contexts/index.ts
@@ -0,0 +1,5 @@
+export { WorkspaceProvider, useWorkspace } from "./WorkspaceContext";
+export type { WorkspaceView } from "./WorkspaceContext";
+export { TabsProvider, useTabsContext } from "./TabsContext";
+export type { DocumentTab, RecentFile, TabsMeta } from "./TabsContext";
+export { FileActionsProvider, useFileActions } from "./FileActionsContext";
